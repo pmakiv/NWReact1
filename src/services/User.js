021903586b625116942885f405608e91f10481d1
pmakiv <p.makiviita@gmail.com>
2026-02-09 14:34:38 +0200

    dasads

--- src/services/User.js
+++ src/services/User.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = "https://localhost:7167/api/Users"
+const baseUrl = "https://northwindrestapi120260209134825-gaczgmcwd6h7cwdw.swedencentral-01.azurewebsites.net/api/Users"
 
 let token = null
 
